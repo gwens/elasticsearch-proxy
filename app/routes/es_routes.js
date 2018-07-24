50b53cef52109525619f43e4c5814341495cb758
gwens <gwenanspearing@hotmail.com>
2018-07-24 17:45:15 +0200

    When the app receives a POST request to /emails/search, it sends a GET request to elasticsearch and console.logs the response status, but doesn't do anything with the response yet.

--- app/routes/es_routes.js
+++ app/routes/es_routes.js
@@ -1,6 +1,11 @@
+const fetch = require('node-fetch');
+
 module.exports = function(app, db) {
   app.post('/emails/search', (req, res) => {
     console.log(req.body);
+    fetch("http://ambient-explorer.ml:9200/_cat/indices?v", {
+      method: 'GET'
+    }).then(res => console.log("status " + res.statusText))
     res.send('received a POST request to /emails/search');
   });
 };
