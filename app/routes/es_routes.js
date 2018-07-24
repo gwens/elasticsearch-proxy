7d0d8813890ad6d1ff847da483492f29e11e6b91
gwens <gwenanspearing@hotmail.com>
2018-07-24 18:19:31 +0200

    Passes on a valid search request to Elasticsearch and console.logs the response but doesn't return it yet.

--- app/routes/es_routes.js
+++ app/routes/es_routes.js
@@ -3,9 +3,14 @@ const fetch = require('node-fetch');
 module.exports = function(app, db) {
   app.post('/emails/search', (req, res) => {
     console.log(req.body);
-    fetch("http://ambient-explorer.ml:9200/_cat/indices?v", {
-      method: 'GET'
-    }).then(res => console.log("status " + res.statusText))
+    const elasticUrl = "https://search-search-archive-sxxeh2lvo7lacugez36nv2f4bq.us-east-2.es.amazonaws.com/emails/_search";
+    fetch(elasticUrl, {
+      method: 'POST',
+      body: JSON.stringify(req.body),
+      headers: {'Content-Type': 'application/json'}
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {console.log(response)});
     res.send('received a POST request to /emails/search');
   });
 };
