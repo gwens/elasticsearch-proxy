7b11935cb3e6dc6e65080a6e26a8942d3c86d076
gwens <gwenanspearing@hotmail.com>
2018-07-25 06:42:03 +0200

    Elasticsearch index is no longer hard-coded but taken from the request params (url)

--- app/routes/es_routes.js
+++ app/routes/es_routes.js
@@ -1,11 +1,13 @@
 const fetch = require('node-fetch');
-const elasticUrl = "https://search-search-archive-sxxeh2lvo7lacugez36nv2f4bq.us-east-2.es.amazonaws.com/emails/_search";
+const elasticUrl = "https://search-search-archive-sxxeh2lvo7lacugez36nv2f4bq.us-east-2.es.amazonaws.com/";
 
 module.exports = function(app, db) {
-  app.post('/emails/search', (req, res) => {
+  app.post('/:index/_search', (req, res) => {
     console.log(req.headers.host);
+    // URL to search the specific Elasticsearch index requested
+    const fullUrl = elasticUrl + req.params.index + "/_search";
     
-    fetch(elasticUrl, {
+    fetch(fullUrl, {
       method: 'POST',
       body: JSON.stringify(req.body),
       headers: {'Content-Type': 'application/json'}
