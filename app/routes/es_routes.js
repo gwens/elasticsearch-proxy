475bca887fd3ae4a4b9ea1d54a4133793b491c81
gwens <gwenanspearing@hotmail.com>
2018-07-25 08:58:08 +0200

    Comments, and commented out debug code.

--- app/routes/es_routes.js
+++ app/routes/es_routes.js
@@ -1,5 +1,8 @@
 const fetch = require('node-fetch');
+
+// Elasticsearch domain...
 const elasticUrl = "https://search-search-archive-sxxeh2lvo7lacugez36nv2f4bq.us-east-2.es.amazonaws.com/";
+// ...and the site that should be able to access it
 const allowedOrigin = "https://ambient-explorer.ml";
 
 module.exports = function(app, db) {
@@ -11,15 +14,15 @@ module.exports = function(app, db) {
   });
 
   // Debug
-  app.get('/', (req, res) => {
+  /*app.get('/', (req, res) => {
     res.send('getting');
-  });
+  });*/
 
   app.post('/:index/_search', (req, res) => {
     console.log(req.headers.host);
     // URL to search the specific Elasticsearch index requested
     const fullUrl = elasticUrl + req.params.index + "/_search";
-    
+    // Forward the request to Elasticsearch and pass back the response
     fetch(fullUrl, {
       method: 'POST',
       body: JSON.stringify(req.body),
